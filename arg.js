3d50e9608bf9be7e63674d9e33922ead75473849
buu-2 <brunoewerthon0328@gmail.com>
2026-07-20 22:47:06 -0300

    Add files via upload

---
+++ arg.js
@@ -0,0 +1,14 @@
+
+
+const verificar = ()=>{
+
+    const senha = document.getElementById("senha").value
+    
+
+    if(senha.toUpperCase() === "FELIX"){
+        document.body.innerHTML=
+        `<img src="ixi.gif"/>`
+    }
+
+
+}
